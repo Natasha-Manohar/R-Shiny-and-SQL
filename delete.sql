c6c3601b91383906f21e82e26ea6ebf63867a3a9
Natasha-Manohar <51310342+Natasha-Manohar@users.noreply.github.com>
2019-06-03 11:01:59 +0530

    Add files via upload

---
+++ delete.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS jobs.Has;
+DROP TABLE IF EXISTS jobs.Apply;
+DROP TABLE IF EXISTS jobs.Skill;
+DROP TABLE IF EXISTS jobs.Candidate;
+DROP TABLE IF EXISTS jobs.Job;
+DROP TABLE IF EXISTS jobs.Organization;
+
+
+
+
